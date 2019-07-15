cd8071a6d6a7735f55f81bd520b2cd0724f86aa8
Chris O'Neil <chris.oneil@gmail.com>
2019-07-15 21:43:32 +0100

    style/jobs: arrange in alphabetical order
    This is the order in which they appear on the Jenkins board, so just
    keep them in the same order in this file so that they are easy to find.

--- job_dsl_seed.groovy
+++ job_dsl_seed.groovy
@@ -1,54 +1,13 @@
-multibranchPipelineJob('pipeline-safe_client_libs') {
-    branchSources {
-        github {
-            checkoutCredentialsId('github_maidsafe_token_credentials')
-            scanCredentialsId('github_maidsafe_token_credentials')
-            repoOwner('maidsafe')
-            repository('safe_client_libs')
-        }
-    }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('scripts/Jenkinsfile')
-        }
-    }
-}
-
-multibranchPipelineJob('pipeline-safe_nd') {
-    branchSources {
-        github {
-            checkoutCredentialsId('github_maidsafe_token_credentials')
-            scanCredentialsId('github_maidsafe_token_credentials')
-            repoOwner('maidsafe')
-            repository('safe-nd')
-        }
-    }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Jenkinsfile')
-        }
-    }
-}
-
-pipelineJob('docker_build-safe_client_libs_build_container') {
+pipelineJob('ami_build-safe_nd_slave') {
     parameters {
-        stringParam('BRANCH', 'experimental')
+        stringParam('BRANCH', 'master')
         stringParam(
             'REPO_URL',
-            'https://github.com/maidsafe/safe_client_libs.git')
+            'https://github.com/maidsafe/safe-build-infrastructure.git')
+        stringParam('SAFE_IMAGE_TAG', '')
     }
 
-    description('Builds and pushes the container for Safe Client Libs')
+    description('Builds and pushes the container for safe-nd')
 
     definition {
         cpsScm {
@@ -56,7 +15,7 @@ pipelineJob('docker_build-safe_client_libs_build_container') {
                 git {
                     remote { url('https://github.com/maidsafe/jenkins-jobs.git') }
                     branches('master')
-                    scriptPath('docker_build-safe_client_libs_build_container/Jenkinsfile')
+                    scriptPath('ami_build-safe_nd_slave/Jenkinsfile')
                     extensions { }
                 }
             }
@@ -88,6 +47,30 @@ pipelineJob('docker_build-safe_cli_build_container') {
     }
 }
 
+pipelineJob('docker_build-safe_client_libs_build_container') {
+    parameters {
+        stringParam('BRANCH', 'experimental')
+        stringParam(
+            'REPO_URL',
+            'https://github.com/maidsafe/safe_client_libs.git')
+    }
+
+    description('Builds and pushes the container for Safe Client Libs')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('https://github.com/maidsafe/jenkins-jobs.git') }
+                    branches('master')
+                    scriptPath('docker_build-safe_client_libs_build_container/Jenkinsfile')
+                    extensions { }
+                }
+            }
+        }
+    }
+}
+
 pipelineJob('docker_build-safe_nd_build_container') {
     parameters {
         stringParam('BRANCH', 'master')
@@ -112,31 +95,6 @@ pipelineJob('docker_build-safe_nd_build_container') {
     }
 }
 
-pipelineJob('ami_build-safe_nd_slave') {
-    parameters {
-        stringParam('BRANCH', 'master')
-        stringParam(
-            'REPO_URL',
-            'https://github.com/maidsafe/safe-build-infrastructure.git')
-        stringParam('SAFE_IMAGE_TAG', '')
-    }
-
-    description('Builds and pushes the container for safe-nd')
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url('https://github.com/maidsafe/jenkins-jobs.git') }
-                    branches('master')
-                    scriptPath('ami_build-safe_nd_slave/Jenkinsfile')
-                    extensions { }
-                }
-            }
-        }
-    }
-}
-
 multibranchPipelineJob('pipeline-safe_cli') {
     branchSources {
         github {
@@ -144,6 +102,48 @@ multibranchPipelineJob('pipeline-safe_cli') {
             scanCredentialsId('github_maidsafe_token_credentials')
             repoOwner('maidsafe')
             repository('safe-cli')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
+
+multibranchPipelineJob('pipeline-safe_client_libs') {
+    branchSources {
+        github {
+            checkoutCredentialsId('github_maidsafe_token_credentials')
+            scanCredentialsId('github_maidsafe_token_credentials')
+            repoOwner('maidsafe')
+            repository('safe_client_libs')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('scripts/Jenkinsfile')
+        }
+    }
+}
+
+multibranchPipelineJob('pipeline-safe_nd') {
+    branchSources {
+        github {
+            checkoutCredentialsId('github_maidsafe_token_credentials')
+            scanCredentialsId('github_maidsafe_token_credentials')
+            repoOwner('maidsafe')
+            repository('safe-nd')
         }
     }
     orphanedItemStrategy {
